81a2b224bea72b5b94d82f4c02458a33e4dc16af
isknergis <isknrgs@gmail.com>
2024-04-29 23:34:29 +0300

    kutuphane_yonetim_sistemi
    C# ve Sql ile oluşturulan Windows Form Uygulaması

---
+++ KütüphaneYönetimSistemi/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace KitapUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKitapAdi = new System.Windows.Forms.TextBox();
+            this.txtYazar = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.listBoxKitaplar = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKitapAdi
+            // 
+            this.txtKitapAdi.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtKitapAdi.Location = new System.Drawing.Point(254, 75);
+            this.txtKitapAdi.Multiline = true;
+            this.txtKitapAdi.Name = "txtKitapAdi";
+            this.txtKitapAdi.Size = new System.Drawing.Size(267, 61);
+            this.txtKitapAdi.TabIndex = 0;
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtYazar.Location = new System.Drawing.Point(254, 196);
+            this.txtYazar.Multiline = true;
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(267, 61);
+            this.txtYazar.TabIndex = 1;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtISBN.Location = new System.Drawing.Point(254, 313);
+            this.txtISBN.Multiline = true;
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(267, 61);
+            this.txtISBN.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox4.Location = new System.Drawing.Point(254, 429);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(267, 61);
+            this.textBox4.TabIndex = 3;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(566, 75);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(160, 55);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Kaydet";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(616, 322);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(566, 196);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(207, 87);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "btnSil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click_1);
+            // 
+            // listBoxKitaplar
+            // 
+            this.listBoxKitaplar.FormattingEnabled = true;
+            this.listBoxKitaplar.ItemHeight = 16;
+            this.listBoxKitaplar.Location = new System.Drawing.Point(40, 480);
+            this.listBoxKitaplar.Name = "listBoxKitaplar";
+            this.listBoxKitaplar.Size = new System.Drawing.Size(183, 84);
+            this.listBoxKitaplar.TabIndex = 7;
+            this.listBoxKitaplar.SelectedIndexChanged += new System.EventHandler(this.listBoxKitaplar_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(1017, 754);
+            this.Controls.Add(this.listBoxKitaplar);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtYazar);
+            this.Controls.Add(this.txtKitapAdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKitapAdi;
+        private System.Windows.Forms.TextBox txtYazar;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ListBox listBoxKitaplar;
+    }
+}
+
---
+++ KütüphaneYönetimSistemi/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace KitapUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        private NpgsqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new NpgsqlConnection("Host=localhost;Port=5432;Username=postgres;Password=nergis;Database=kutuphane;");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshKitapListesi();
+        }
+
+        private void RefreshKitapListesi()
+        {
+            try
+            {
+                conn.Open();
+                string query = "SELECT id, kitap_adi FROM kitaplar WHERE mevcut = true";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                List<Tuple<int, string>> kitaplar = new List<Tuple<int, string>>();
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["id"]);
+                    string kitapAdi = reader["kitap_adi"].ToString();
+                    kitaplar.Add(new Tuple<int, string>(id, kitapAdi));
+                }
+
+                listBoxKitaplar.DisplayMember = "Item2";
+                listBoxKitaplar.ValueMember = "Item1";
+                listBoxKitaplar.DataSource = kitaplar;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+     
+
+
+        private void listBoxKitaplar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEkle_Click_1(object sender, EventArgs e)
+        {
+
+            string kitapAdi = txtKitapAdi.Text;
+            string yazar = txtYazar.Text;
+            string isbn = txtISBN.Text;
+
+            try
+            {
+                conn.Open();
+                string query = "INSERT INTO kitaplar (kitap_adi, yazar, mevcut, \"ISBN\") VALUES (@kitapAdi, @yazar, true, @isbn)";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@kitapAdi", kitapAdi);
+                cmd.Parameters.AddWithValue("@yazar", yazar);
+                cmd.Parameters.AddWithValue("@isbn", isbn);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Kitap başarıyla eklendi.");
+                    RefreshKitapListesi();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnSil_Click_1(object sender, EventArgs e)
+        {
+            if (listBoxKitaplar.SelectedIndex != -1)
+            {
+                int kitapId = (listBoxKitaplar.SelectedItem as Tuple<int, string>).Item1;
+                try
+                {
+                    conn.Open();
+                    string query = "DELETE FROM kitaplar WHERE id = @id";
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", kitapId);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Kitap başarıyla silindi.");
+                        RefreshKitapListesi();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek istediğiniz bir kitap seçin.");
+            }
+        }
+    }
+}
